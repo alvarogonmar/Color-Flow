932782eea861aa41ac270c11e11f7374e51d4520
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-02 22:54:46 -0600

    Adding pygame.QUIT

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ import pygame
 pygame.init() #START PYGAME
 
 screen = pygame.display.set_mode((800, 600)) #DISPLAY THE SCREEN
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: #QUIT GAME
+            running = False
