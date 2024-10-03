946171b1041788ee27cd86ad001e3eeaa226d92f
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-03 10:37:48 -0600

    Adding the class for the game

--- main.py
+++ main.py
@@ -9,15 +9,19 @@ screen = pygame.display.set_mode((800, 600)) #DISPLAY THE SCREEN
 pygame.display.set_caption('Color Flow')
 icon = pygame.image.load('/Users/alvarogonzalez/Documents/PROGRAMMING/Color-Flow/simon_logo.png') #APP ICON
 pygame.display.set_icon(icon)
-background = pygame.image.load('') #GAME BACKGROUND
+# background = pygame.image.load('') #GAME BACKGROUND
 
 # ADD BACKGROUND MUSIC
-mixer.music.load('')
-mixer.music.play(-1)  # -1 means it repeats every time it ends
+# mixer.music.load('')
+# mixer.music.play(-1)  # -1 means it repeats every time it ends
 
 #GAME LOOP
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: #QUIT GAME
-            running = False
+class Game():
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: #QUIT GAME
+                running = False
+
+game1 = Game()
