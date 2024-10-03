2f8015263296200bc94dcc6b5a267acb978e8c71
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-02 22:57:36 -0600

    Adding the title

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ pygame.init() #START PYGAME
 
 screen = pygame.display.set_mode((800, 600)) #DISPLAY THE SCREEN
 
+#TITLE AND ICON
+pygame.display.set_caption('Color Flow')
+
+#GAME LOOP
 running = True
 while running:
     for event in pygame.event.get():
