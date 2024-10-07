32a80e14d26cb8e8dea0c965ba3bdf71bafd64fe
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-06 21:33:58 -0600

    Changing the order

--- main.py
+++ main.py
@@ -3,23 +3,23 @@ from pygame import mixer
 from settings import *
 
 
-pygame.init() #START PYGAME
-
-screen = pygame.display.set_mode((800, 600)) #DISPLAY THE SCREEN
-
-#TITLE AND ICON
-pygame.display.set_caption('Color Flow')
-icon = pygame.image.load('/Users/alvarogonzalez/Documents/PROGRAMMING/Color-Flow/simon_logo.png') #APP ICON
-pygame.display.set_icon(icon)
-# background = pygame.image.load('') #GAME BACKGROUND
-
-# ADD BACKGROUND MUSIC
-mixer.music.load('/Users/alvarogonzalez/Documents/PROGRAMMING/Color-Flow/background_music.wav')
-mixer.music.play(-1)  # -1 means it repeats every time it ends
 
 #GAME LOOP
 class Game():
+     
+     #START PYGAME
+    pygame.init()
 
+    screen = pygame.display.set_mode((800, 600)) #DISPLAY THE SCREEN
+    #TITLE AND ICON
+    pygame.display.set_caption('Color Flow')
+    icon = pygame.image.load('/Users/alvarogonzalez/Documents/PROGRAMMING/Color-Flow/simon_logo.png') #APP ICON
+    pygame.display.set_icon(icon)
+    # background = pygame.image.load('') #GAME BACKGROUND
+
+    # ADD BACKGROUND MUSIC
+    # mixer.music.load('/Users/alvarogonzalez/Documents/PROGRAMMING/Color-Flow/background_music.wav')
+    # mixer.music.play(-1)  # -1 means it repeats every time it ends
     running = True
     while running:
         for event in pygame.event.get():
