763a79deac81f0d82fce6bf666afbb86e881d328
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-07 19:13:51 -0600

    Adding new changes to def __init__

--- main.py
+++ main.py
@@ -1,29 +1,52 @@
 import pygame
 from pygame import mixer
 from settings import *
+from sprites import *
 
 
 
 #GAME LOOP
 class Game():
-     
-     #START PYGAME
-    pygame.init()
+    def __init__(self):
+        #START PYGAME
+        pygame.init()
+        screen = pygame.display.set_mode((640, 500)) #DISPLAY THE SCREEN
+        #TITLE AND ICON
+        pygame.display.set_caption('Color Flow')
+        icon = pygame.image.load('/Users/alvarogonzalez/Documents/PROGRAMMING/Color-Flow/simon_logo.png') #APP ICON
+        pygame.display.set_icon(icon)
+        # FPS
+        self.clock = pygame.time.Clock() 
+        self.running = True
 
-    screen = pygame.display.set_mode((800, 600)) #DISPLAY THE SCREEN
-    #TITLE AND ICON
-    pygame.display.set_caption('Color Flow')
-    icon = pygame.image.load('/Users/alvarogonzalez/Documents/PROGRAMMING/Color-Flow/simon_logo.png') #APP ICON
-    pygame.display.set_icon(icon)
-    # background = pygame.image.load('') #GAME BACKGROUND
+        # background = pygame.image.load('') #GAME BACKGROUND
 
-    # ADD BACKGROUND MUSIC
-    # mixer.music.load('/Users/alvarogonzalez/Documents/PROGRAMMING/Color-Flow/background_music.wav')
-    # mixer.music.play(-1)  # -1 means it repeats every time it ends
-    running = True
-    while running:
+        # ADD BACKGROUND MUSIC
+        # mixer.music.load('/Users/alvarogonzalez/Documents/PROGRAMMING/Color-Flow/background_music.wav')
+        # mixer.music.play(-1)  # -1 means it repeats every time it ends
+    def run(self):
+
+        while self.running:
+            self.clock.tick(FPS)  
+            self.events()  
+            self.draw() 
+    
+    def events(self):
         for event in pygame.event.get():
-            if event.type == pygame.QUIT: #QUIT GAME
-                running = False
+            if event.type == pygame.QUIT:  # Salir si se cierra la ventana
+                self.running = False
+
+    def draw(self):
+        self.screen.fill(DARKGREY)  
+        pygame.display.update()  
+
+
+
+        # while running:
+        #     for event in pygame.event.get():
+        #         if event.type == pygame.QUIT: #QUIT GAME
+        #             running = False
 
 game1 = Game()
+game1.run()
+pygame.quit()
