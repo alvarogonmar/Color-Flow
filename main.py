b780d06f7087ccf50ae3f6958b4bd93207f1027c
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-07 19:19:12 -0600

    Adding new def

--- main.py
+++ main.py
@@ -2,51 +2,44 @@ import pygame
 from pygame import mixer
 from settings import *
 from sprites import *
-
+import random
 
 
 #GAME LOOP
 class Game():
     def __init__(self):
-        #START PYGAME
-        pygame.init()
+        pygame.init() #START PYGAME
         screen = pygame.display.set_mode((640, 500)) #DISPLAY THE SCREEN
+
         #TITLE AND ICON
         pygame.display.set_caption('Color Flow')
         icon = pygame.image.load('/Users/alvarogonzalez/Documents/PROGRAMMING/Color-Flow/simon_logo.png') #APP ICON
         pygame.display.set_icon(icon)
+
         # FPS
         self.clock = pygame.time.Clock() 
         self.running = True
 
-        # background = pygame.image.load('') #GAME BACKGROUND
-
         # ADD BACKGROUND MUSIC
         # mixer.music.load('/Users/alvarogonzalez/Documents/PROGRAMMING/Color-Flow/background_music.wav')
         # mixer.music.play(-1)  # -1 means it repeats every time it ends
-    def run(self):
-
-        while self.running:
-            self.clock.tick(FPS)  
-            self.events()  
-            self.draw() 
     
-    def events(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:  # Salir si se cierra la ventana
-                self.running = False
+    def new(self):
+        pass
+    
+    def run(self):
+        pass
+
+    def update(self):
+        pass
 
     def draw(self):
-        self.screen.fill(DARKGREY)  
-        pygame.display.update()  
+        pass
 
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit(0) #FINISH THE PROGRAM
 
-
-        # while running:
-        #     for event in pygame.event.get():
-        #         if event.type == pygame.QUIT: #QUIT GAME
-        #             running = False
-
-game1 = Game()
-game1.run()
-pygame.quit()
+    
